81b44635e4b5d5cbab992fcc533bb673b650a1f9
EdgarAlvarez-ROl <edgaralvares4208@gmail.com>
2024-05-27 23:51:12 -0600

    set command voices more naturals

--- js/speech.js
+++ js/speech.js
@@ -1,13 +1,20 @@
 
 
 var num = 0;
-const fraseRec2 = "ACTUALIZAR DOS";
-const fraseRec3 = "ACTUALIZAR TRES";
-const fraseRec4 = "ACTUALIZAR CUATRO";
-const fraseRec5 = "ACTUALIZAR 5";
-const fraseRec6 = "ACTUALIZAR SEIS";
-const fraseRec7 = "ACTUALIZAR 7";
-const fraseRec8 = "ACTUALIZAR 8";
+const fraseRec2 = "ACTUALIZAR A DOS";
+const fraseRec2_2 = "ACTUALIZAR A 2";
+const fraseRec3 = "ACTUALIZAR A TRES";
+const fraseRec3_2 = "ACTUALIZAR A 3";
+const fraseRec4 = "ACTUALIZAR A CUATRO";
+const fraseRec4_2 = "ACTUALIZAR A 4";
+const fraseRec5 = "ACTUALIZAR A 5";
+const fraseRec5_2 = "ACTUALIZAR A CINCO";
+const fraseRec6 = "ACTUALIZAR A SEIS";
+const fraseRec6_2 = "ACTUALIZAR A 6";
+const fraseRec7 = "ACTUALIZAR A 7";
+const fraseRec7_2 = "ACTUALIZAR A SIETE";
+const fraseRec8 = "ACTUALIZAR A 8";
+const fraseRec8_2 = "ACTUALIZAR A OCHO";
 
 const fraseGirar = "JARVIS GIRALA";
 
@@ -50,9 +57,9 @@ if (!SpeechRecognition) {
             setTimeout(() => {
                 spin();
             }, 2400);
-        } else if (transcript === 'ACTUALIZAR') {
-            updateOptions();
-        } else if (transcript === 'JARVIS PLAY') {
+        // } else if (transcript === 'ACTUALIZAR') {
+        //     updateOptions();
+        } else if (transcript === 'JARVIS PONELE PLAY') {
             playVideo();
         // NOTA
         // Cambiar por Switch
@@ -61,27 +68,27 @@ if (!SpeechRecognition) {
             document.getElementById("numOptions").value = "2";
             var numOptions = parseInt(document.getElementById("numOptions").value);
             updateOptions();
-        } else if (transcript.includes(fraseRec3)) {
+        } else if (transcript.includes(fraseRec3) || transcript.includes(fraseRec3_2)) {
             console.log("frase 3");
             document.getElementById("numOptions").value = "3";
             updateOptions();
-        } else if (transcript.includes(fraseRec4)) {
+        } else if (transcript.includes(fraseRec4) || transcript.includes(fraseRec4_2)) {
             console.log("frase 4");
             document.getElementById("numOptions").value = "4";
             updateOptions();
-        } else if (transcript.includes(fraseRec5)) {
+        } else if (transcript.includes(fraseRec5) || transcript.includes(fraseRec5_2)) {
             console.log("frase 5");
             document.getElementById("numOptions").value = "5";
             updateOptions();
-        } else if (transcript.includes(fraseRec6)) {
+        } else if (transcript.includes(fraseRec6) || (transcript.includes(fraseRec6_2))) {
             console.log("frase 6");
             document.getElementById("numOptions").value = "6";
             updateOptions();
-        } else if (transcript.includes(fraseRec7)) {
+        } else if (transcript.includes(fraseRec7) || transcript.includes(fraseRec7_2)) {
             console.log("frase 7");
             document.getElementById("numOptions").value = "7";
             updateOptions();
-        } else if (transcript.includes(fraseRec8)) {
+        } else if (transcript.includes(fraseRec8) || transcript.includes(fraseRec8_2)) {
             console.log("frase 8");
             document.getElementById("numOptions").value = "8";
             updateOptions();
